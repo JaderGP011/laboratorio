4751ee6d43a2d4346b68fd141c4fe1cb0140577a
JaderGP011 <jadergamaperes@gmail.com>
2023-10-06 10:46:40 -0300

    Quiz update

--- quiz.js
+++ quiz.js
@@ -1,5 +1,23 @@
-function acao() {
-    if (condition) {
+let resp = document.querySelector("input#V, input#F")
+let acerto
+let erro
+function vVF() {
+    if (resp == 1) {
+        acerto++
         
     }
+}
+
+function calcularResultado() {
+    const respostas = document.querySelector("input#V, input#F");
+    let pontuacao = 0;
+
+    for (const resposta of respostas) {
+        if (resposta.value === "Paris" || resposta.value === "Júpiter") {
+            pontuacao++;
+        }
+    }
+
+    const resultado = document.getElementById('result');
+    resultado.textContent = `Você acertou ${pontuacao} de ${respostas.length} perguntas.`;
 }
--- quiz.py
+++ quiz.py
@@ -1,25 +1,45 @@
-acerto = int(0)
-erro = int(0)
-#Funções de verificação
+acerto = 0
+erro = 0
 
-#verdadeiro Ou falso
-def vVF(resp):
+def vV(resp): #Verficação Verdadeiro
     global acerto, erro
     if resp == 1:
-        acerto = acerto + 1
+        acerto += 1
         print('Acertou!')
     elif resp == 2:
-        erro = erro + 1
+        erro += 1
         print('Errou!')
     else:
-        print('Resposta inválida!')
+        print('Responda apenas com 1 ou 2')
 
-print('HTML é uma linguagem de marcação? ')
-resp = int(input('1 = Verdadeiro, 2 = Falso '))
-vVF(resp)
+def vF(resp): #Verificação falso
+    global acerto, erro
+    if resp == 1:
+        erro += 1
+        print('Acertou!')
+    elif resp == 2:
+        acerto += 1
+        print('Errou!')
+    else:
+        print('Responda apenas com 1 ou 2')
+
+print('HTML é uma linguagem de marcação?')
+resp = int(input('1 = Verdadeiro, 2 = Falso: '))
+vV(resp)
 
 print('O personagem Chaves é de um seriado de mesmo nome?')
-resp = int(input('1 = Verdadeiro, 2 = Falso '))
-vVF(resp)
-print("Você acertou {} perguntas e errou {}".format(acerto, erro))
+resp = int(input('1 = Verdadeiro, 2 = Falso: '))
+vV(resp)
+
+print("Os Emirados Árabes estão no continente:\n 1) Americano\n 2) asiatico\n 3) Europeu")
+
+
+print('Qual a Capital dos Estados Unidos?')
+print('1) Nova York')
+print('2) Orlando')
+print('3) Washington, D.C')
+resp = int(input('Digite o número da sua escolha: '))
+vME(resp)
+
+print(f"Você acertou {acerto} perguntas e errou {erro}.")
 
